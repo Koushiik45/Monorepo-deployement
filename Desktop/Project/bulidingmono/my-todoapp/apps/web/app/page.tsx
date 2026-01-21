511d210ec8ca6c564638417496cad00f9ef29ca2
koushii45 <104384890+koushii45@users.noreply.github.com>
2026-01-20 20:44:35 -0500

    DockerFiles added

--- Desktop/Project/bulidingmono/my-todoapp/apps/web/app/page.tsx
+++ Desktop/Project/bulidingmono/my-todoapp/apps/web/app/page.tsx
@@ -1,7 +1,9 @@
+import type { ReactNode } from "react";
 import { prismaClient } from "db/client";
 
-export default async function Home() {
+export default async function Home(): Promise<ReactNode> {
   const users = await prismaClient.user.findMany();
+
   return (
     <div>
       {JSON.stringify(users)}
@@ -9,3 +11,8 @@ export default async function Home() {
   );
 }
 
+// how to convert the static page to dynamic
+
+export const revalidate = 60 //revalidate every 60 seconds
+//or
+//export const dynamic =  'force-dynamic'
